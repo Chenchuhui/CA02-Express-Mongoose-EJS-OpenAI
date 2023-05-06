4f6d83dc59cc4bf07bdcfb0749375bd0229305e4
Chenchuhui <chehu@ucdavis.edu>
2023-05-05 22:44:34 -0400

    Update the project by Chenchuhui

--- routes/openai.js
+++ routes/openai.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const Fortune = require('../models/Fortune');
+const User = require('../models/User');
+
 const { Configuration, OpenAIApi } = require("openai");
+const { isLoggedIn } = require('./pwauth');
 console.log(process.env.OPENAI_API_KEY);
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -17,14 +21,35 @@ const get_response = async(prompt) => {
 
 };
 
-router.get('/fortune', (req, res, next) => {
-    res.render('fortune');
-})
+router.get('/fortune',
+    isLoggedIn,
+    async(req, res, next) => {
+        response = await Fortune.findOne({ userId: req.user._id });
+        if (response) {
+            res.locals.response = response.item;
+        } else {
+            res.locals.response = "Haven't got one yet!"
+        }
+
+        res.render('fortune');
+    }
+)
 
 router.post('/fortune',
+    isLoggedIn,
     async(req, res, next) => {
         console.log('getting fortune');
-        res.locals.response = await get_response("Generate a fortune");
+        res.locals.response = await get_response("Please generate a fortune! Give a warm message directly!");
+        response = await Fortune.findOneAndUpdate({ userId: req.user._id }, { $set: { item: res.locals.response } });
+        console.log(response);
+        if (!response) {
+            const fortune = new Fortune({
+                item: res.locals.response,
+                userId: req.user._id
+            })
+            await fortune.save();
+        };
+
         res.render('fortuneResult');
     }
 )
